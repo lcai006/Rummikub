c1541bdcc941a8a36c787209ad5a4d3b5b93c5c6
Luwei <lcai006@uottawa.ca>
2021-09-20 16:56:35 -0400

    CODE FOR deck creation

--- src/main/java/model/Deck.java
+++ src/main/java/model/Deck.java
@@ -1,12 +1,27 @@
 package model;
 
-public class Deck {
-    public Deck() {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Deck {
+    private ArrayList<String> deck = new ArrayList<>();
+
+    public Deck() {
+        // deck is a list of tiles in string
+        String[] colors = {"R", "G", "B", "O"};
+        for (String color: colors) {
+            for (int i = 1; i <=13; i++) {
+                deck.add(color+i);
+                deck.add(color+i);
+            }
+        }
+
+        // Randomize the deck
+        Collections.shuffle(deck);
     }
 
     public int size() {
-        return 0;
+        return deck.size();
     }
 
 }
