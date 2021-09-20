161bb7adf3a8de938ac7d74a23437fe5f46d1b1e
Luwei <lcai006@uottawa.ca>
2021-09-20 17:43:50 -0400

    CODE FOR hand list creation from the deck with and without predefined tiles

--- src/main/java/model/Deck.java
+++ src/main/java/model/Deck.java
@@ -10,7 +10,7 @@ public class Deck {
         // deck is a list of tiles in string
         String[] colors = {"R", "G", "B", "O"};
         for (String color: colors) {
-            for (int i = 1; i <=13; i++) {
+            for (int i = 1; i <= 13; i++) {
                 deck.add(color+i);
                 deck.add(color+i);
             }
@@ -27,6 +27,30 @@ public class Deck {
     // Generate hand list with or without predefined tiles, predefined tiles are used for testing
     public ArrayList<String> createHand(String predefined) {
         ArrayList<String> hand = new ArrayList<>();
+
+        if (predefined.equals("")) {
+            // draws 14 tiles
+            for (int i = 0; i < 14; i++) {
+                if (deck.size() > 0) {
+                    hand.add(deck.get(0));
+                    deck.remove(0);
+                }
+            }
+        } else {
+            // adds predefined tiles
+            String[] tiles = predefined.split("\\s+");
+            for (String tile : tiles) {
+                hand.add(tile);
+                deck.remove(tile);
+            }
+            // draws left tiles
+            for (int i = 0; i < 14 - tiles.length; i++) {
+                if (deck.size() > 0) {
+                    hand.add(deck.get(0));
+                    deck.remove(0);
+                }
+            }
+        }
 
         return hand;
     }
