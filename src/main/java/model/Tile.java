f70ed1c178ac25fa3b6d8abb5fc15a19c08ef5c4
Luwei <lcai006@uottawa.ca>
2021-09-21 05:09:15 -0400

    CODE FOR tile object, attributes and display in text

--- src/main/java/model/Tile.java
+++ src/main/java/model/Tile.java
@@ -1,31 +1,52 @@
 package model;
 
 public class Tile {
-    public Tile(String val) {
+    private final String str;
+    private final String color;
+    private final int number;
+    private boolean newHighlighted = false;
+    private boolean moveHighlighted = false;
 
+    public Tile(String val) {
+        color = val.substring(0, 1);
+        if (val.length() == 2)
+            number = Integer.parseInt(val.substring(1, 2));
+        else
+            number = Integer.parseInt(val.substring(1, 3));
+
+        str = val;
     }
 
     public String color() {
-        return "";
+        return this.color;
     }
 
     public int number() {
-        return 0;
+        return this.number;
     }
 
     public void newHighlight() {
-
+        newHighlighted = true;
     }
 
     public void moveHighlight() {
-
+        moveHighlighted = true;
     }
 
     public void removeHighlight() {
-
+        newHighlighted = false;
+        moveHighlighted = false;
     }
 
     public String toString() {
-        return "";
+        String text;
+        if (newHighlighted)
+            text = "*" + str;
+        else if (moveHighlighted)
+            text = "!" + str;
+        else
+            text = str;
+
+        return text;
     }
 }
