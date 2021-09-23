530e6cee31aaa531eeab80f8d39c13e2a4a89ec1
Luwei <lcai006@uottawa.ca>
2021-09-23 01:39:47 -0400

    CODE FOR table object, modification of melds in table, and display in text

--- src/main/java/model/Table.java
+++ src/main/java/model/Table.java
@@ -1,24 +1,39 @@
 package model;
 
-public class Table {
-    public Table() {
+import java.util.ArrayList;
 
+public class Table {
+    private ArrayList<Meld> melds;
+    public Table() {
+        melds = new ArrayList<>();
     }
 
     public boolean isEmpty() {
-        return true;
+        return melds.isEmpty();
     }
 
     public void createMeld(String list) {
-
+        Meld m = new Meld(list);
+        melds.add(m);
     }
 
     public void addTile(int num, String list) {
-
+        melds.get(num - 1).add(list);
     }
 
     public void removeTile(int num, String list) {
+        melds.get(num - 1).remove(list);
+    }
 
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        int i = 0;
+        for (Meld m: melds) {
+            i++;
+            str.append("Meld ").append(i).append(": ");
+            str.append(m.toString()).append("\n");
+        }
+        return str.toString();
     }
 
 
